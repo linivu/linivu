32790f1dd7c74ff6f781a005ef9c1a3f36ad393f
Ivan Kleynhans <info@linivu.com>
2021-08-05 10:43:05 +0200

    Update App component: add react lazy, suspense and import Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { lazy, Suspense } from 'react';
+
+// COMPONENTS //
+import Loader from './components/global/Loader';
+const Header = lazy(() => import('./components/layout/Header'));
+
 // STYLES //
 import './App.scss';
 
 const App = () => {
-  return <h1>Linivu React Application</h1>;
+  return (
+    <Suspense fallback={<Loader />}>
+      <Header />
+    </Suspense>
+  );
 };
 
 export default App;
