584a8c0bde981bc9242088cbb9f7e3f48f068223
Ivan Kleynhans <info@linivu.com>
2021-08-05 14:08:56 +0200

    Update Navigation component: add onClickHandler and Button component

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,19 +1,29 @@
+// COMPONENTS //
+import Button from '../global/Button';
+
 // STYLES //
 import './Navigation.scss';
 
 const Navigation = ({ isOpen, setIsOpen }) => {
   const navLinks = ['home', 'about', 'services', 'works'];
 
+  const onClickHandler = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className={`nav ${isOpen ? 'open' : ''}`}>
       <ul className={`nav__list ${isOpen ? 'open' : ''}`}>
-        {navLinks.map((link) => (
-          <li>
-            <a href={`#${link}`} className="nav__link">
+        {navLinks.map((link, i) => (
+          <li key={i}>
+            <a href={`#${link}`} className="nav__link" onClick={onClickHandler}>
               {link}
             </a>
           </li>
         ))}
+        <Button href="mailto:info@linivu.com" className="btn btn--primary">
+          Get In Touch
+        </Button>
       </ul>
     </nav>
   );
