4545126dea942bf60e593e8064492673698f892b
Ivan Kleynhans <info@linivu.com>
2021-08-05 15:30:05 +0200

    Update App component: add LandingPage component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from 'react';
 // COMPONENTS //
 import Loader from './components/global/Loader';
 const Header = lazy(() => import('./components/layout/Header'));
+const LandingPage = lazy(() => import('./pages/LandingPage'));
 
 // STYLES //
 import './App.scss';
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <Suspense fallback={<Loader />}>
       <Header />
+      <LandingPage />
     </Suspense>
   );
 };
