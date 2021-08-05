07f079ca7fd620618ca15275c8546c2e049005ac
Ivan Kleynhans <info@linivu.com>
2021-08-05 10:41:16 +0200

    Add Loader component

---
+++ src/components/global/Loader.jsx
@@ -0,0 +1,16 @@
+// STYLES //
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="dots dots--fade">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
