5e88fd5b03c4a4ebe276b0c7f7d1c40b94202c12
Ivan Kleynhans <info@linivu.com>
2021-08-05 12:52:16 +0200

    Update Header component: add Navigation component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,6 +6,7 @@ import HamburgerMenu from './HamburgerMenu';
 
 // STYLES //
 import './Header.scss';
+import Navigation from './Navigation';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,6 +14,7 @@ const Header = () => {
     <header className="header">
       <div className="header__container">
         <Logo />
+        <Navigation isOpen={isOpen} setIsOpen={setIsOpen} />
         <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
     </header>
