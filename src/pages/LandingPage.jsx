cb0c297df3038641ec48c3d59e4c13a01633efd8
Ivan Kleynhans <info@linivu.com>
2021-08-07 09:03:53 +0200

    Update LandingPage component: add About

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 // COMPONENTS //
 import Home from '../components/page/Home';
+import About from '../components/page/About';
 
 const LandingPage = () => {
   return (
     <main className="main">
       <Home />
+      <About />
     </main>
   );
 };
