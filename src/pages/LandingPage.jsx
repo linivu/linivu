ca79002e76dac1388468abd6a54c6e96e48f5161
Ivan Kleynhans <info@linivu.com>
2021-08-07 12:52:16 +0200

    Update LanddingPage component: add useEffect and stickyNavbar

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
+import stickyNavbar from '../utilities/stickyNavbar';
+
 // COMPONENTS //
 import Home from '../components/page/Home';
 import About from '../components/page/About';
 
 const LandingPage = () => {
+  useEffect(() => {
+    stickyNavbar();
+  });
+
   return (
     <main className="main">
       <Home />
