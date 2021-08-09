dacf32c43877264f401ad92f01f5c1244daff3f7
Ivan Kleynhans <info@linivu.com>
2021-08-09 08:30:58 +0200

    Add About component data

---
+++ src/data/index.js
@@ -0,0 +1,45 @@
+const data = {
+  about: {
+    title: 'Who We Are',
+
+    mainTitle:
+      'Linivu is a small web design studio based in Cape Town. We are dedicated to building great digital products and experiences.',
+
+    lead: "We take a structured approach to web design. Our development process was created to ensure every project is delivered on-time and on-budget. Once your web design project kicks off, here's what to expect:",
+
+    process: [
+      {
+        no: 1,
+        heading: 'Define',
+        description:
+          'The first order of business is to sit down with your team and create a detailed set of design and technical specifications. These specifications serve as a road map for the rest of the web design process.',
+      },
+      {
+        no: 2,
+        heading: 'Design',
+        description:
+          "Wire frames are your first chance to visualize your website. While they're not nearly as detailed as the final site will be, they give us a visual representation of the site's overall layout.",
+      },
+      {
+        no: 3,
+        heading: 'Build',
+        description:
+          "At the same time, we'll kick off the technical side of the web design process. This will include deploying your CMS, creating your custom theme and page designs, and setting up your website's analytics.",
+      },
+      {
+        no: 4,
+        heading: 'Launch',
+        description:
+          "Once we're sure that your site is ready to be released to the public we'll deploy it on your public domain. Then, we'll shift into a monthly support process that will continue for 12 months.",
+      },
+    ],
+  },
+
+  services: {
+    title: '',
+    mainTitle: '',
+    lead: '',
+  },
+};
+
+export default data;
