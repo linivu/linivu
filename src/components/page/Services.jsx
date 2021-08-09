d53554c589fe3ef1ad7cdfa39dade04b2585ca2a
Ivan Kleynhans <info@linivu.com>
2021-08-09 10:07:55 +0200

    Add Services comonent and styles

---
+++ src/components/page/Services.jsx
@@ -0,0 +1,39 @@
+// DATA //
+import data from '../../data';
+
+// IMAGES //
+import design from '../../images/icons/design.svg';
+import development from '../../images/icons/development.svg';
+import seo from '../../images/icons/seo.svg';
+
+// STYLES //
+import './Services.scss';
+
+const Services = () => {
+  return (
+    <section id="services" className="services">
+      <div className="container">
+        <div className="section-title" data-num="02">
+          <h2 className="section-title__description">{data.services.title}</h2>
+        </div>
+
+        <div className="grid">
+          <h3 className="main-title">{data.services.mainTitle}</h3>
+          <p className="lead">{data.services.lead}</p>
+        </div>
+
+        <div className="grid">
+          {data.services.items.map((item) => (
+            <div className="service__item" key={item.no}>
+              <img src={item.icon} className="services__item-icon"></img>
+              <h2 className="services__item-heading">{item.heading}</h2>
+              <p className="services__item-description">{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
