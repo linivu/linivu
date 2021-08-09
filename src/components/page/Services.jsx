676ccea072d1a53c1750bc198db8883627fa98fe
Ivan Kleynhans <info@linivu.com>
2021-08-09 13:34:50 +0200

    Update Services component: remove img tag and replace with div background image

--- src/components/page/Services.jsx
+++ src/components/page/Services.jsx
@@ -1,16 +1,10 @@
 // DATA //
 import data from '../../data';
 
-import design from '../../images/icons/design.svg';
-import branding from '../../images/icons/branding.svg';
-import development from '../../images/icons/development.svg';
-import seo from '../../images/icons/seo.svg';
-
 // STYLES //
 import './Services.scss';
 
 const Services = () => {
-  // console.log(design);
   return (
     <section id="services" className="services">
       <div className="container">
@@ -26,11 +20,11 @@ const Services = () => {
         <div className="grid">
           {data.services.items.map((item) => (
             <div className="service__item" key={item.no}>
-              <img
-                src={item.icon}
-                alt={item.heading}
-                className="services__item-icon"
-              ></img>
+              <div
+                className={`services__item-icon ${item.heading
+                  .split(' ')[0]
+                  .toLowerCase()}`}
+              ></div>
               <h2 className="services__item-heading">{item.heading}</h2>
               <p className="services__item-description">{item.description}</p>
             </div>
