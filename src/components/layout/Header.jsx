b924510976ab461fc42cd22c15833f37eb3c0801
Ivan Kleynhans <info@linivu.com>
2021-08-10 08:04:02 +0200

    Update Header component: move Navigation component to components

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react';
 
 // COMPONENTS //
 import Logo from '../global/Logo';
+import Navigation from './Navigation';
 import HamburgerMenu from './HamburgerMenu';
 
 // STYLES //
 import './Header.scss';
-import Navigation from './Navigation';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="header__container">
