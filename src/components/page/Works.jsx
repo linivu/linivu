3f9ccbcc8e52cd89a17daa0b632e8c6a72db6fcc
Ivan Kleynhans <info@linivu.com>
2021-08-11 13:37:03 +0200

    Add Works component

---
+++ src/components/page/Works.jsx
@@ -0,0 +1,60 @@
+import discovr from '../../images/discovr.png';
+import omnifood from '../../images/omnifood.png';
+
+// STYLES //
+import './Works.scss';
+
+const Works = () => {
+  return (
+    <section id="works" className="works">
+      <div className="container">
+        <div className="grid">
+          <div className="section-title" data-num="03">
+            <h2 className="section-title__description">Recent Works</h2>
+          </div>
+          <h3 className="main-title">
+            Here are some of our projects we have done lately. Feel free to check them
+            out.
+          </h3>
+        </div>
+
+        <div className="grid">
+          <div className="works__item">
+            <img src={discovr} alt="works item img" />
+            <div className="overlay"></div>
+            <div className="works__item-intro">
+              <p className="type">Web App</p>
+              <p className="name">Discovr</p>
+            </div>
+            <a
+              href="https://discovr.linivu.com"
+              target="_blank"
+              rel="noopener"
+              className="works__item-preview"
+            >
+              Preview
+            </a>
+          </div>
+          <div className="works__item">
+            <img src={omnifood} alt="works item img" />
+            <div className="overlay"></div>
+            <div className="works__item-intro">
+              <p className="type">Website</p>
+              <p className="name">Omnifood</p>
+            </div>
+            <a
+              href="https://omnifood.linivu.com"
+              target="_blank"
+              rel="noopener"
+              className="works__item-preview"
+            >
+              Preview
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Works;
