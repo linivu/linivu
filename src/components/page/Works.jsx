8d5f9d43fdbbd90279eb1a254313839c69d3e6fa
Ivan Kleynhans <info@linivu.com>
2021-08-11 15:45:09 +0200

    Update Works component: add jpg images

--- src/components/page/Works.jsx
+++ src/components/page/Works.jsx
@@ -1,6 +1,6 @@
 import data from '../../data';
-import discovr from '../../images/discovr.png';
-import omnifood from '../../images/omnifood.png';
+import discovr from '../../images/discovr.jpg';
+import omnifood from '../../images/omnifood.jpg';
 
 // STYLES //
 import './Works.scss';
