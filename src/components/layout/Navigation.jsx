f58ac5a02d7231c742ecd6d5f40f911738105d78
Ivan Kleynhans <info@linivu.com>
2021-08-12 10:30:49 +0200

    Update Navigation component: add plane svg

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -30,6 +30,20 @@ const Navigation = ({ isOpen, setIsOpen }) => {
           className="btn btn--primary link"
           onClick={onClickHandler}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="plane"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+            />
+          </svg>
           Get In Touch
         </Button>
       </div>
