4b763b446cda18cba00101aacb3a19ca06b7b49e
Ivan Kleynhans <info@linivu.com>
2021-08-12 11:22:12 +0200

    Update About component: add animation

--- src/components/page/About.jsx
+++ src/components/page/About.jsx
@@ -7,20 +7,20 @@ const About = () => {
   return (
     <section id="about" className="about">
       <div className="container">
-        <div className="section-title" data-num="01">
+        <div className="section-title" data-num="01" data-animate="fade-up">
           <h2 className="section-title__description">{data.about.title}</h2>
         </div>
 
         <div className="grid">
-          <div className="about__intro">
+          <div className="about__intro" data-animate="fade-up">
             <h3 className="main-title">{data.about.mainTitle}</h3>
             <p className="lead">{data.about.lead}</p>
           </div>
-          <div className="about__image"></div>
+          <div className="about__image" data-animate="fade-up"></div>
         </div>
         <div className="grid">
           {data.about.process.map((process) => (
-            <div className="about__process" key={process.no}>
+            <div className="about__process" key={process.no} data-animate="fade-up">
               <div className="about__process-box">
                 <div className="about__process-number">{process.no}</div>
                 <h3 className="about__process-heading">{process.heading}</h3>
