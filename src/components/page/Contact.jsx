19d1f7684e242cc572b75d96960a20c6e01b7cf6
Ivan Kleynhans <info@linivu.com>
2021-08-12 11:22:33 +0200

    Update Contact component: add animation

--- src/components/page/Contact.jsx
+++ src/components/page/Contact.jsx
@@ -5,15 +5,19 @@ const Contact = () => {
   return (
     <section id="contact" className="contact">
       <div className="container">
-        <div className="section-title" data-num="04">
+        <div className="section-title" data-num="04" data-animate="fade-up">
           <h2 className="section-title__description">Get In Touch</h2>
         </div>
 
-        <h2 className="main-title">
+        <h2 className="main-title" data-animate="fade-up">
           Have an idea or an epic project in mind? Talk to us. Let us work together and
           make something great. Shoot us a message at
         </h2>
-        <a href="mailto:info@linivu.com" className="contact__email">
+        <a
+          href="mailto:info@linivu.com"
+          className="contact__email"
+          data-animate="fade-up"
+        >
           info@linivu.com
         </a>
       </div>
